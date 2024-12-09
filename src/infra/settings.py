60782a552d23dcd6493831239d4a3c768fa50181
guilherme <vinespedro55@gmail.com>
2024-12-09 18:09:40 -0300

    fix: import url database

--- src/infra/settings.py
+++ src/infra/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Replace the DATABASES section of your settings.py with this
-tmpPostgres =os.getenv("DATABASE_URL")
+tmpPostgres =urlparse(os.getenv("DATABASE_URL"))
 
 
 # Quick-start development settings - unsuitable for production
